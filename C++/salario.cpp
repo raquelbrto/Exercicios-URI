ac50f7b49bebdaaed9e24f31e8be9f6ebbc322cb
Raquel Alves <51912497+RaquelABrito@users.noreply.github.com>
2020-11-13 23:03:30 -0300

    Questao salario 10008

--- C++/salario.cpp
+++ C++/salario.cpp
@@ -3,19 +3,15 @@
 
 using namespace std;
 
-int main (){
-    int numFuncionario,horasTrabalhadas;
-    double salario,valorHora;
-    cin>>numFuncionario>>horasTrabalhadas>>valorHora;
-    cout<<fixed<<setprecision(2);
+int main()
+{
+    int numFuncionario, horasTrabalhadas;
+    double salario, valorHora;
+    cin >> numFuncionario >> horasTrabalhadas >> valorHora;
+    cout << fixed << setprecision(2);
     salario = horasTrabalhadas * valorHora;
-    cout<<"NUMBER = "<< numFuncionario<<endl;
-    cout<<"SALARY = $ "<<setprecision(2)<<salario<< endl;
-
-
-
-
-
+    cout << "NUMBER = " << numFuncionario << endl;
+    cout << "SALARY = U$ " << setprecision(2) << salario << endl;
 
     return 0;
 }
